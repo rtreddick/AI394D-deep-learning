1ab8d016ff22522db393ccd2395912db9e3811c4
Tim Reddick <r.t.reddick@gmail.com>
2025-02-02 13:51:36 -0500

    finish hw 1 nnc

---
+++ deeplearning/homework_01/__init__.py
@@ -0,0 +1,9 @@
+from deeplearning.homework_01.homework.pytorch_basics import PyTorchBasics
+from deeplearning.homework_01.homework.nearest_neighbor_classifier import NearestNeighborClassifier
+from deeplearning.homework_01.homework.weather_forecast import WeatherForecast
+
+__all__ = [
+    'PyTorchBasics',
+    'NearestNeighborClassifier',
+    'WeatherForecast'
+]
